d0bd5292fca9e8f5fd55e0d7ea485e50617252a9
SupriseB <suprisebaloyi17@gmail.com>
2025-06-06 22:43:36 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,6 @@
 
 # Prompting user for a positive integer
-size = int(input("Enter the size of the pattern e.g.,square: "))
+size = int(input("Enter the size of the pattern: "))
 
 if size <= 0:
     print("Please enter a positive integer.")
