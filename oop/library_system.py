33c21af0fab818a95c9dc61d17c3a49ddd399ccc
SupriseB <suprisebaloyi17@gmail.com>
2025-06-18 10:40:37 +0200

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,19 +1,15 @@
 #Task Description:
 #Develop two Python scripts: library_system.py and main.py. In library_system.py, you’ll define a base class Book and two derived classes, EBook and PrintBook, showcasing inheritance. Additionally, implement a Library class demonstrating composition by managing a collection of books.
-
 #library_system.py:
 #Base Class - Book:
-
 #Attributes: title (str) and author (str).
 #Method: __init__(self, title, author) for initializing book attributes.
 #Derived Classes - EBook and PrintBook:
-
 #Both classes inherit from Book.
 #EBook additional attribute: file_size (int).
 #PrintBook additional attribute: page_count (int).
 #Each derived class should have its own __init__ method that properly calls the base class __init__ while also initializing its unique attribute.
 #Composition - Library:
-
 #Attributes: books (a list to store instances of Book, EBook, and PrintBook).
 #Methods:
 #add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
@@ -28,7 +24,7 @@ class Book:
     def get_details(self):
         return f"Title: {self.title}, Author: {self.author}"
 
-class EBook (Book):
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author) #Calling parent attribute
         self.file_size = file_size
@@ -37,7 +33,7 @@ class EBook (Book):
         return f"EBook - {super().get_details()}, File Size: {self.file_size}
    
 
-class PrintBook (Book):
+class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author) #Calling parent attribute
         self.page_count = page_count
